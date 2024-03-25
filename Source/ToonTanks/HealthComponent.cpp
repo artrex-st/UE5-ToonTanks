e76ba21dc2267e4b5848f7f63e7d940c19e07916
Arthur Stefanelli <artrexwow@gmail.com>
2024-03-25 16:00:19 -0300

    add Health Component

---
+++ Source/ToonTanks/HealthComponent.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+
+UHealthComponent::UHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	Health = MaxHealth;
+}
+
+
+void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
---
+++ Source/ToonTanks/HealthComponent.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TOONTANKS_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UHealthComponent();
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;		
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	float MaxHealth = 100.f;
+	float Health = 0.f;
+};
